fb391e671849cf83c1d05ed0172be9a37f8e619e
harinarayananlk-ai <hari.narayanan.lk@gmail.com>
2026-07-24 16:16:56 +0530

    fix(web): prevent CSSStyleDeclaration indexed property setter error in react-native-svg and reanimated

--- scripts/patch-webshape.js
+++ scripts/patch-webshape.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-const modulePath = path.join(__dirname, '../node_modules/react-native-svg/lib/module/web/WebShape.js');
-const commonjsPath = path.join(__dirname, '../node_modules/react-native-svg/lib/commonjs/web/WebShape.js');
+// 1. Patch react-native-svg WebShape
+const svgModulePath = path.join(__dirname, '../node_modules/react-native-svg/lib/module/web/WebShape.js');
+const svgCommonjsPath = path.join(__dirname, '../node_modules/react-native-svg/lib/commonjs/web/WebShape.js');
 
-const moduleContent = `function safeAssignStyle(targetStyle, styleProp) {
-  if (!styleProp) return;
+const svgModuleContent = `function safeAssignStyle(targetStyle, styleProp) {
+  if (!styleProp || !targetStyle) return;
   if (Array.isArray(styleProp)) {
-    for (const item of styleProp) {
-      safeAssignStyle(targetStyle, item);
+    for (let i = 0; i < styleProp.length; i++) {
+      safeAssignStyle(targetStyle, styleProp[i]);
     }
   } else if (typeof styleProp === 'object') {
     for (const key of Object.keys(styleProp)) {
@@ -22,13 +23,13 @@ const moduleContent = `function safeAssignStyle(targetStyle, styleProp) {
 }
 import React from 'react';
 import {
-// @ts-ignore it is not seen in exports
-unstable_createElement as createElement } from 'react-native';
+  unstable_createElement as createElement } from 'react-native';
 import { prepare } from './utils/prepare';
 import { convertInt32ColorToRGBA } from './utils/convertInt32Color';
 import { camelCaseToDashed, remeasure } from './utils';
 import { hasTouchableProperty } from './utils/hasProperty';
 import SvgTouchableMixin from '../lib/SvgTouchableMixin';
+
 export class WebShape extends React.Component {
   prepareProps(props) {
     return props;
@@ -36,9 +37,6 @@ export class WebShape extends React.Component {
   elementRef = /*#__PURE__*/React.createRef();
   lastMergedProps = {};
 
-  /**
-   * disclaimer: I am not sure why the props are wrapped in a \`style\` attribute here, but that's how reanimated calls it
-   */
   setNativeProps(props) {
     let styleObj = {};
     if (props && props.style) {
@@ -62,7 +60,9 @@ export class WebShape extends React.Component {
           case 'children':
             break;
           case 'style':
-            safeAssignStyle(current.style, clean.style);
+            if (current.style) {
+              safeAssignStyle(current.style, clean.style);
+            }
             break;
           case 'fill':
             if (cleanValue && typeof cleanValue === 'object') {
@@ -77,7 +77,9 @@ export class WebShape extends React.Component {
             }
             break;
           default:
-            current.setAttribute(camelCaseToDashed(cleanAttribute), cleanValue);
+            try {
+              current.setAttribute(camelCaseToDashed(cleanAttribute), cleanValue);
+            } catch(e) {}
             break;
         }
       }
@@ -85,8 +87,6 @@ export class WebShape extends React.Component {
   }
   constructor(props) {
     super(props);
-
-    // Do not attach touchable mixin handlers if SVG element doesn't have a touchable prop
     if (hasTouchableProperty(props)) {
       SvgTouchableMixin(this);
     }
@@ -94,20 +94,16 @@ export class WebShape extends React.Component {
   }
   render() {
     if (!this.tag) {
-      throw new Error('When extending \`WebShape\` you need to overwrite either \`tag\` or \`render\`!');
+      throw new Error('When extending WebShape you need to overwrite either tag or render!');
     }
     this.lastMergedProps = {};
     return createElement(this.tag, prepare(this, this.prepareProps(this.props)));
   }
 }
-//# sourceMappingURL=WebShape.js.map
 `;
 
-const commonjsContent = `"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+const svgCommonjsContent = `"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.WebShape = void 0;
 var _react = _interopRequireDefault(require("react"));
 var _reactNative = require("react-native");
@@ -119,10 +115,10 @@ var _SvgTouchableMixin = _interopRequireDefault(require("../lib/SvgTouchableMixi
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 
 function safeAssignStyle(targetStyle, styleProp) {
-  if (!styleProp) return;
+  if (!styleProp || !targetStyle) return;
   if (Array.isArray(styleProp)) {
-    for (const item of styleProp) {
-      safeAssignStyle(targetStyle, item);
+    for (let i = 0; i < styleProp.length; i++) {
+      safeAssignStyle(targetStyle, styleProp[i]);
     }
   } else if (typeof styleProp === 'object') {
     for (const key of Object.keys(styleProp)) {
@@ -142,9 +138,6 @@ class WebShape extends _react.default.Component {
   elementRef = /*#__PURE__*/_react.default.createRef();
   lastMergedProps = {};
 
-  /**
-   * disclaimer: I am not sure why the props are wrapped in a \`style\` attribute here, but that's how reanimated calls it
-   */
   setNativeProps(props) {
     let styleObj = {};
     if (props && props.style) {
@@ -168,7 +161,9 @@ class WebShape extends _react.default.Component {
           case 'children':
             break;
           case 'style':
-            safeAssignStyle(current.style, clean.style);
+            if (current.style) {
+              safeAssignStyle(current.style, clean.style);
+            }
             break;
           case 'fill':
             if (cleanValue && typeof cleanValue === 'object') {
@@ -183,7 +178,9 @@ class WebShape extends _react.default.Component {
             }
             break;
           default:
-            current.setAttribute((0, _utils.camelCaseToDashed)(cleanAttribute), cleanValue);
+            try {
+              current.setAttribute((0, _utils.camelCaseToDashed)(cleanAttribute), cleanValue);
+            } catch(e) {}
             break;
         }
       }
@@ -191,7 +188,6 @@ class WebShape extends _react.default.Component {
   }
   constructor(props) {
     super(props);
-
     if ((0, _hasProperty.hasTouchableProperty)(props)) {
       (0, _SvgTouchableMixin.default)(this);
     }
@@ -199,16 +195,109 @@ class WebShape extends _react.default.Component {
   }
   render() {
     if (!this.tag) {
-      throw new Error('When extending \`WebShape\` you need to overwrite either \`tag\` or \`render\`!');
+      throw new Error('When extending WebShape you need to overwrite either tag or render!');
     }
     this.lastMergedProps = {};
     return (0, _reactNative.unstable_createElement)(this.tag, (0, _prepare.prepare)(this, this.prepareProps(this.props)));
   }
 }
 exports.WebShape = WebShape;
-//# sourceMappingURL=WebShape.js.map
 `;
 
-fs.writeFileSync(modulePath, moduleContent, 'utf8');
-fs.writeFileSync(commonjsPath, commonjsContent, 'utf8');
-console.log('Successfully written clean WebShape files!');
+if (fs.existsSync(svgModulePath)) fs.writeFileSync(svgModulePath, svgModuleContent, 'utf8');
+if (fs.existsSync(svgCommonjsPath)) fs.writeFileSync(svgCommonjsPath, svgCommonjsContent, 'utf8');
+
+// 2. Patch react-native-reanimated js-reanimated index.js
+const reanimatedModulePath = path.join(__dirname, '../node_modules/react-native-reanimated/lib/module/ReanimatedModule/js-reanimated/index.js');
+const reanimatedCommonjsPath = path.join(__dirname, '../node_modules/react-native-reanimated/lib/commonjs/ReanimatedModule/js-reanimated/index.js');
+
+const reanimatedModuleContent = `'use strict';
+import { logger } from '../../common';
+import { createReactDOMStyle, createTextShadowValue, createTransformValue } from './webUtils';
+export { createJSReanimatedModule } from './JSReanimated';
+
+export const _updatePropsJS = (updates, viewRef, isAnimatedProps) => {
+  if (viewRef) {
+    const component = viewRef.getAnimatableRef ? viewRef.getAnimatableRef() : viewRef;
+    const [rawStyles] = Object.keys(updates).reduce((acc, key) => {
+      if (!isNaN(Number(key))) return acc;
+      const value = updates[key];
+      const index = typeof value === 'function' ? 1 : 0;
+      acc[index][key] = value;
+      return acc;
+    }, [{}, {}]);
+    if (typeof component.setNativeProps === 'function') {
+      setNativeProps(component, rawStyles, isAnimatedProps);
+    } else if (createReactDOMStyle !== undefined && component.style !== undefined) {
+      updatePropsDOM(component, rawStyles, isAnimatedProps);
+    } else if (component.props && Object.keys(component.props).length > 0) {
+      Object.keys(component.props).forEach(key => {
+        if (!isNaN(Number(key)) || !rawStyles[key]) return;
+        const dashedKey = key.replace(/[A-Z]/g, m => '-' + m.toLowerCase());
+        try { component._touchableNode?.setAttribute(dashedKey, rawStyles[key]); } catch (e) {}
+      });
+    }
+  }
+};
+
+const setNativeProps = (component, newProps, isAnimatedProps) => {
+  const cleanProps = {};
+  if (newProps && typeof newProps === 'object') {
+    for (const k of Object.keys(newProps)) {
+      if (isNaN(Number(k))) {
+        cleanProps[k] = newProps[k];
+      }
+    }
+  }
+  if (isAnimatedProps) {
+    try { component.setNativeProps?.(cleanProps); } catch (e) {}
+  }
+  const previousStyle = component.previousStyle ? component.previousStyle : {};
+  const currentStyle = {
+    ...previousStyle,
+    ...cleanProps
+  };
+  component.previousStyle = currentStyle;
+  try {
+    component.setNativeProps?.({
+      style: currentStyle
+    });
+  } catch (e) {}
+};
+
+const updatePropsDOM = (component, style, isAnimatedProps) => {
+  const previousStyle = component.previousStyle ? component.previousStyle : {};
+  const currentStyle = {
+    ...previousStyle,
+    ...style
+  };
+  component.previousStyle = currentStyle;
+  const domStyle = createReactDOMStyle(currentStyle);
+  if (Array.isArray(domStyle.transform) && createTransformValue !== undefined) {
+    domStyle.transform = createTransformValue(domStyle.transform);
+  }
+  if (createTextShadowValue !== undefined && (domStyle.textShadowColor || domStyle.textShadowRadius || domStyle.textShadowOffset)) {
+    domStyle.textShadow = createTextShadowValue({
+      textShadowColor: domStyle.textShadowColor,
+      textShadowOffset: domStyle.textShadowOffset,
+      textShadowRadius: domStyle.textShadowRadius
+    });
+  }
+  for (const key in domStyle) {
+    if (!isNaN(Number(key))) continue;
+    if (isAnimatedProps) {
+      if (component.nodeName === 'INPUT' && key === 'text') {
+        component.value = domStyle[key];
+      } else {
+        try { component.setAttribute(key, domStyle[key]); } catch (e) {}
+      }
+    } else {
+      try { component.style[key] = domStyle[key]; } catch (e) {}
+    }
+  }
+};
+`;
+
+if (fs.existsSync(reanimatedModulePath)) fs.writeFileSync(reanimatedModulePath, reanimatedModuleContent, 'utf8');
+
+console.log('Successfully applied web SVG & Reanimated patches!');
